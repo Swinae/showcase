e85d8939d08433f5424d9059841c3e890cf3f571
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-08-26 15:53:18 +0200

    set up router in main

--- FRONT/src/main.tsx
+++ FRONT/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
