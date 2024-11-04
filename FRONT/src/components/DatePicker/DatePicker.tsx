18c7b29c4ca6e116b3db8477dea18dda68d4a9b8
Alexandre BALPE <alexandre.balpe@gmail.com>
2024-11-04 16:41:30 +0100

    Added: - Working date picker

--- FRONT/src/components/DatePicker/DatePicker.tsx
+++ FRONT/src/components/DatePicker/DatePicker.tsx
@@ -1,54 +1,82 @@
 import { css } from '@emotion/react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker, RangeKeyDict } from 'react-date-range';
+import { useState } from 'react';
 
 interface DatePickerProps {
-    dates: {
-        start_date: Date | undefined,
-        end_date: Date | undefined
-    }
+    newTripDates: (data: any) => void;
+}
+
+interface DateRange {
+    startDate: Date | undefined;
+    endDate: Date | undefined;
+    key: string | undefined;
 }
 
 export function DatePicker(props: DatePickerProps) {
-    const { dates } = props
 
-    const formatDate = (date: Date | undefined) => {
-        return date
-            ? date.toLocaleDateString('en-gb', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-            })
-            : 'Date not available'; // Fallback text when date is undefined
-    };
-
-    const start_date = formatDate(dates.start_date)
-    const end_date = formatDate(dates.end_date)
-
-
+    const { newTripDates } = props
     /* ---STYLE--- */
     const datePickerStyle = css`
-        padding: 1.5rem 0rem;
-        box-shadow: 0px 0px 15px -2px rgba(155, 155, 155, 0.40);
-        border-radius: 3rem;
-        text-align: center;
-        font-family: 'Oswalt', sans-serif;
-        font-size: 20px;
-        font-weight: 500;
+        .rdrDateRangePickerWrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-        & input {
-            border: none;
-            font-family: 'Oswalt', sans-serif;
-            font-size: 20px;
-            font-weight: 500; 
+        .rdrCalendarWrapper.rdrDateRangeWrapper{
+            flex: 1;
+        }
+
+        .rdrMonth {
+            width: 100%;
+        }
+        
+        .rdrDefinedRangesWrapper {
+            display: none
         }
     `
 
-    return (
-        <div>
-            <div className='date-picker' css={datePickerStyle}>
-                <i className="fa-solid fa-calendar-days"></i> 
-                <input className='date-picker-input' type="date" value={start_date}/> <i className="fa-solid fa-arrow-right"></i> <input className='date-picker-input' type="date" value={end_date}/>
-            </div>
-        </div>
+    const [dateRange, setDateRange] = useState<DateRange[]>([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection',
+        },
+    ]);
 
+    const handleSelect = (item: RangeKeyDict) => {
+        setDateRange([
+            {
+                startDate: item.selection.startDate,
+                endDate: item.selection.endDate,
+                key: 'selection'
+            }
+        ])
+    }
+
+    return (
+        <>
+            <DateRangePicker
+                css={datePickerStyle}
+                ranges={dateRange}
+                onChange={ (item) => {
+                    handleSelect(item)
+                    newTripDates(item)             
+                }}
+                months={1}
+                direction="vertical"
+                showDateDisplay={false}
+                rangeColors={['var(--third-font-color)']}
+                moveRangeOnFirstSelection={false}
+            />
+        </>
+        /*      <div>
+                 <div className='date-picker' css={datePickerStyle}>
+                     <i className="fa-solid fa-calendar-days"></i> 
+                     <input className='date-picker-input' type="date" value={start_date}/> <i className="fa-solid fa-arrow-right"></i> <input className='date-picker-input' type="date" value={end_date}/>
+                 </div>
+             </div> */
     )
 }
--- FRONT/src/pages/TripDetails/TripDetails.tsx
+++ FRONT/src/pages/TripDetails/TripDetails.tsx
@@ -3,12 +3,23 @@ import { useParams } from "react-router-dom"
 import { getTripInfo } from "../../services/api/Trips"
 import { Trip } from "../../services/interfaces/Trip"
 import { DatePicker } from "../../components/DatePicker/DatePicker"
+import { css } from "@emotion/react"
+
+interface TripDates {
+    selection: {
+        startDate: Date,
+        endDate: Date,
+        key: string
+    }
+}
 
 export function TripDetailsPage() {
 
-    const {tripId} = useParams()
+    const { tripId } = useParams()
     const [tripInfo, setTripInfo] = useState<Trip>()
+    const [tripDates, setTripDates] = useState<TripDates>()
 
+    /* LOAD TRIP INFO */
     useEffect(() => {
         const loadTrips = async () => {
             try {
@@ -22,10 +33,68 @@ export function TripDetailsPage() {
         loadTrips()
     }, [tripId])
 
+
+    /* DATE RANGE */
+    const handleTripDates = (dates: TripDates): void => {
+        setTripDates(dates)
+    }
+
+    const [newStartDate, setNewStartDate] = useState<string | undefined>('')
+    const [newEndDate, setNewEndDate] = useState<string | undefined>('')
+
+    useEffect(() => {
+        setNewStartDate(tripDates?.selection.startDate.toLocaleDateString())
+        setNewEndDate(tripDates?.selection.endDate.toLocaleDateString())
+    }, [tripDates])
+
+    /* HANDLE CALANDAR VISIBILITY */
+    const [showCalandar, setShowCalandar] = useState(false)
+    const toggleCalandarVisibility = () => {
+        setShowCalandar(!showCalandar)
+    }
+
+    /*---STYLE--- */
+    const calandarDisplay = css`
+        .displayNone {
+            display: none;
+        }
+
+        .displayBlock {
+            display: block;
+        }
+    `
+
+    const dateDisplayStyle = css`
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        width: 100%;
+        color: #1d1d1d;
+        
+        & input {
+            font-size: 20px;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            width: 100px;
+            color: #1d1d1d; //TO CHECK: COLOR IS NOT TAKEN INTO ACCOUNT FOR SOME REASON
+        }
+    `
+
     return (
         <>
             <h1>{tripInfo?.title}</h1>
-            <DatePicker dates = {{start_date: tripInfo?.start_date, end_date: tripInfo?.end_date}}/>
+            <div className="date-display-wrapper" css={dateDisplayStyle}>
+                <input readOnly type="text" placeholder={tripDates ? `${newStartDate}` : 'Start'} onClick={toggleCalandarVisibility} alt="Select the starting date of your trip" />
+                <i className="fa-solid fa-arrow-right"></i>
+                <input readOnly type="text" placeholder={tripDates ? `${newEndDate}` : 'End'} onClick={toggleCalandarVisibility} alt="Select the ending date of your trip" />
+            </div>
+
+            <div css={calandarDisplay}>
+                <div className={showCalandar ? "displayBlock" : "displayNone"}>
+                    <DatePicker newTripDates={handleTripDates} />
+                </div>
+            </div>
             <p>{tripInfo?.location}</p>
         </>
     )
