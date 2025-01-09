144324df384b5031b784e0147dca28e8810914cc
Alexandre BALPE <alexandre.balpe@gmail.com>
2025-01-09 14:14:06 +0100

    Added : - Prisma schema and connection to mongo DB

---
+++ BACK/prisma/seeds/seed.mjs
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import { ObjectId } from "mongodb";
+
+const prisma = new PrismaClient();
+
+async function generateUsers() {
+    // Generate 20 users
+    
+    
+    for (let i = 0; i < 20; i++) {
+        const user = await prisma.user.create({
+            data: {
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                role: "USER",
+                trips: {
+                    create: Array.from({ length: 3 }, () => ({
+                        title: faker.lorem.words(3),
+                        location: faker.location.city(),
+                        image: faker.image.url(),
+                        startDate: faker.date.future(),
+                        endDate: faker.date.future(),
+                        activities: Array.from({ length: 20 }, () => ({
+                            id: new ObjectId().toString(), // Unique ID for activities
+                            title: faker.lorem.words(2),
+                            location: faker.location.streetAddress(),
+                            date: faker.date.future(),
+                        })),
+                    })),
+                },
+            },
+        });
+
+        console.log(`Created user: ${user.email}`);
+    }
+}
+
+generateUsers()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
---
+++ BACK/src/trips/trips.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TripsService } from './trips.service';
+import { CreateTripDto } from './dto/create-trip.dto';
+import { UpdateTripDto } from './dto/update-trip.dto';
+
+@Controller('trips')
+export class TripsController {
+  constructor(private readonly tripsService: TripsService) {}
+
+  @Post()
+  create(@Body() createTripDto: CreateTripDto) {
+    return this.tripsService.create(createTripDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.tripsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.tripsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTripDto: UpdateTripDto) {
+    return this.tripsService.update(+id, updateTripDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.tripsService.remove(+id);
+  }
+}
