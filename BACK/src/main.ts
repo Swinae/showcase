b41a30a935a85f9ff7570191e1aaf35368000c05
Alexandre BALPE <alexandre.balpe@gmail.com>
2025-02-04 22:16:18 +0100

    Added : - CORS parameters

--- BACK/src/main.ts
+++ BACK/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',  // Allow requests from your frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,  // Allow cookies and authorization headers
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
